a35504781391107915da16b190059e33edf2ec00
Ben Rush <brush@ocuvera.onmicrosoft.com>
2020-08-07 15:59:08 -0500

    Works

---
+++ buildcopy.ps1
@@ -0,0 +1,5 @@
+#docker build -t brush/gstreamer_android .
+#docker run -dt --rm --name gstreamer_android -v ${PWD}:/android brush/gstreamer_android
+docker exec gstreamer_android /bin/bash -c "cd /android; gradle assembleDebug;"
+D:\android\android-sdk\platform-tools\adb.exe push ${PWD}\app\build\outputs\apk\debug\app-debug.apk /sdcard/Download/app-debug.apk
+docker-compose up
