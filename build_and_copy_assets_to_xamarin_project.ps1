fbd5f13d14e5afcb3f6081674c68cae96b8fbad7
Ben Rush <brush@ocuvera.onmicrosoft.com>
2020-08-18 09:55:25 -0500

    add platform tools.

--- build_and_copy_assets_to_xamarin_project.ps1
+++ build_and_copy_assets_to_xamarin_project.ps1
@@ -5,4 +5,5 @@ docker run -dt --rm --name gstreamer_android -v ${PWD}:/android brush/gstreamer_
 docker exec gstreamer_android /bin/bash -c "cd /android; gradle clean assembleDebug;"
 Start-Sleep 2
 cp .\app\build\intermediates\transforms\stripDebugSymbol\debug\0\lib\armeabi-v7a\*.so .\Xamarin\Xamarin.Android\Libraries\armeabi-7a
+cp .\app\build\intermediates\transforms\stripDebugSymbol\debug\0\lib\x86\*.so .\Xamarin\Xamarin.Android\Libraries\android-x86
 docker-compose up
--- build_and_copy_javaapk_to_connected_phone.ps1
+++ build_and_copy_javaapk_to_connected_phone.ps1
@@ -2,6 +2,6 @@ cd docker
 docker build -t brush/gstreamer_android .
 docker run -dt --rm --name gstreamer_android -v ${PWD}:/android brush/gstreamer_android
 docker exec gstreamer_android /bin/bash -c "cd /android; gradle assembleDebug;"
-D:\android\android-sdk\platform-tools\adb.exe install ${PWD}\..\app\build\outputs\apk\debug\app-debug.apk
+..\platform-tools\adb.exe install ${PWD}\..\app\build\outputs\apk\debug\app-debug.apk
 cd ..
 docker-compose up
