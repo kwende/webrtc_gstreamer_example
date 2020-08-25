cb433cd5e21110d54ec8b3a720ccb5dde081e9d4
Ben Rush <brush@ocuvera.onmicrosoft.com>
2020-08-25 14:42:51 -0500

    Push

--- build_and_copy_assets_to_xamarin_project.ps1
+++ build_and_copy_assets_to_xamarin_project.ps1
@@ -1,5 +1,4 @@
-cd Docker
-docker build -t brush/gstreamer_android .
+docker build -t brush/gstreamer-1.16-build-environment . .
 cd ..
 docker run -dt --rm --name gstreamer_android -v ${PWD}:/android brush/gstreamer_android
 docker exec gstreamer_android /bin/bash -c "cd /android; gradle clean assembleDebug;"
